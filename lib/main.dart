772574e5d623523ed21fd2eb2be53fa849defe9c
mahdiabouzeid <mahdiabouzeid602@gmail.com>
2024-09-06 15:35:31 +0300

    add 2nd welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pharmasea/welcomescreen.dart';
+import 'welcomescreen-2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PrescriptionUploadScreen(),
+      home: GetMedicalProductsScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
